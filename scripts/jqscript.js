9086134dc260edb39b712a0672365d696f87f562
unknown <x.diamondt@CHI233WDIAMWIN7.na.corp.yr.com>
2013-06-04 20:21:54 -0500

    10th upload

--- scripts/jqscript.js
+++ scripts/jqscript.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
-
+   $("#colorChangeInput").change(function() {
+      $("td:first-child").css("backgroundColor", $("#colorChangeInput").val());
+	  $("th").css("backgroundColor", $("#colorChangeInput").val());
+   });
 });
